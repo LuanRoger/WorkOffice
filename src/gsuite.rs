5fa6f0019ac2be3e7800e53b41b8058162fdf4a7
LuanRoger <luan.roger.2003@gmail.com>
2021-05-02 16:05:14 -0300

    v0.2
    Correções:
    - Refatoração no código.

---
+++ src/gsuite.rs
@@ -0,0 +1,45 @@
+use webbrowser;
+
+mod consts {
+    pub const DOC_URL: &str = "http://doc.new";
+    pub const SHEETS_URL: &str = "http://sheet.new";
+    pub const SLIDES_URL: &str = "http://slide.new";
+    pub const FORMS_URL: &str = "http://form.new";
+}
+pub fn open_doc() {
+
+    if webbrowser::open(consts::DOC_URL).is_ok() {
+        println!("Opening Docs...");
+    }
+    else { panic!("Could not start Doc"); }
+}
+pub fn open_sheets() {
+    
+
+    if webbrowser::open(consts::SHEETS_URL).is_ok() {
+        println!("Opening Sheets...");
+    }
+    else {
+        println!("Error");
+    }
+}
+pub fn open_slides() {
+    
+
+    if webbrowser::open(consts::SLIDES_URL).is_ok() {
+        println!("Opening Slides...");
+    }
+    else {
+        println!("Error");
+    }
+}
+pub fn open_forms() {
+    
+
+    if webbrowser::open(consts::FORMS_URL).is_ok() {
+        println!("Opening Forms...");
+    }
+    else {
+        println!("Error");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 use clap::{Arg, App};
-mod doc;
-mod sheets;
-mod slides;
-mod forms;
+mod gsuite;
 
 fn main() {
     let matches = App::new("GsuiteCLI")
-                          .version("0.1")
+                          .version("0.2")
                           .author("LuanRoger <luan.roger.2003@gmail.com>")
                           .about("Shortcut to GSuite tools in terminal")
                           .arg(Arg::with_name("TOOL")
@@ -15,10 +12,10 @@ fn main() {
                           .get_matches();
 
     match matches.value_of("TOOL").unwrap() {
-        "docs" | "doc" => doc::open_doc(),
-        "sheets" | "sheet" => sheets::open_sheets(),
-        "slides" | "slide" => slides::open_slides(),
-        "forms" | "form" => forms::open_forms(),
+        "docs" | "doc" => gsuite::open_doc(),
+        "sheets" | "sheet" => gsuite::open_sheets(),
+        "slides" | "slide" => gsuite::open_slides(),
+        "forms" | "form" => gsuite::open_forms(),
         _ => panic!("This argument is not valid"),
     }
 }
